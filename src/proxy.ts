179a5ccbbd625372cc87897add021b61c01d2ad6
Shikhar Srivastav <shikharsrivastav017@gmail.com>
2026-04-03 17:44:43 +0530

    token in proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,7 +3,13 @@ import { NextRequest,NextResponse } from "next/server";
 import {getToken} from "next-auth/jwt";
 
 export default async function proxy(request:NextRequest){
-    const token=await getToken({req:request,secret:process.env.AUTH_SECRET})
+    const token = await getToken({
+     req: request,
+     secret: process.env.AUTH_SECRET,
+     salt: process.env.NODE_ENV === "production"
+       ? "__Secure-authjs.session-token"
+       : "authjs.session-token"
+   })
     const url=request.nextUrl
 
     if(token && 
@@ -18,12 +24,12 @@ export default async function proxy(request:NextRequest){
     }
 }
 
-export const config={
+export const config={    // config for the middleware,they specify the paths that the middleware should run on
     matcher:[
       "/",
       "/sign-up",
       "/sign-in",
-      "/dashboard/:path*",
+      "/dashboard/:path*",     //
       "/verify/:path*"
     ]
 }
